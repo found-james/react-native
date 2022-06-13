b167eed449f9006b2b42c58c05d4a626da60d780
James Suescun <james.steven2318@gmail.com>
2022-06-12 23:59:03 -0400

    create ListScreen component

---
+++ src/screens/ListScreen.js
@@ -0,0 +1,12 @@
+import React from "react"
+import {View, Text, StyleSheet} from "react-native"
+
+function ListScreen() {
+  return (
+    <Text>ListScreen</Text>
+  )
+}
+
+const styles = StyleSheet.create({})
+
+export default ListScreen
