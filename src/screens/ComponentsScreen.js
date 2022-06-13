2ac3369471bb4dec641d82a95182a4c9d9ca73bc
James Suescun <james.steven2318@gmail.com>
2022-06-12 23:50:39 -0400

    complete first exercise

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -1,15 +1,24 @@
 import React from "react"
-import {Text, StyleSheet} from "react-native"
+import {Text, View, StyleSheet} from "react-native"
 
 function ComponentsScreen() {
-  return (
-    <Text style={styles.textStyle}>This is the components screen</Text>
+              
+    const subHeader = <Text style={styles.subHeader}>My name is WHAT</Text>
+
+    return (
+        <View>
+              <Text style={styles.textStyle}>Getting started with React Native!</Text>
+              {subHeader}
+        </View>
   )
 }
 
 const styles = StyleSheet.create({
     textStyle: {
-        fontSize: 30
+        fontSize: 45
+    },
+    subHeader: {
+        fontSize: 20
     }
 })
 
