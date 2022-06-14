715e4358393ed28794260fa50219cfb0508e98db
James Suescun <james.steven2318@gmail.com>
2022-06-14 18:16:18 -0400

    create button using touchableOpacity component

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,11 +1,14 @@
 import React from "react";
-import { Text, StyleSheet, View, Button } from "react-native";
+import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
 const HomeScreen = () => {
   return (
     <View>
       <Text style={styles.text}>DeKabra</Text>
       <Button title="Go to components demo" onPress={()=> console.log("pressed")}/>
+      <TouchableOpacity onPress={()=> console.log("touchableOpButton")}>
+          <Text>Go to list demo</Text>
+      </TouchableOpacity>
     </View>
   )
 };
