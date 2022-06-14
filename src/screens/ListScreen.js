ddbaf41830a9a5a80a2cd5089386f1d9abc8d9f2
James Suescun <james.steven2318@gmail.com>
2022-06-14 17:50:33 -0400

    complete checkpoint exercise

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -3,18 +3,18 @@ import {View, Text, StyleSheet, FlatList} from "react-native"
 
 function ListScreen() {
   const friends = [
-    {name: "friend one"},
-    {name: "friend two"},
-    {name: "friend three"},
-    {name: "friend four"},
-    {name: "friend five"},
-    {name: "friend six"},
-    {name: "friend seven"},
+    {name: "friend one", age: "one"},
+    {name: "friend two", age: "two"},
+    {name: "friend three", age: "three"},
+    {name: "friend four", age: "four"},
+    {name: "friend five", age: "five"},
+    {name: "friend six", age: "six"},
+    {name: "friend seven", age: "seven"},
   ]
   return (
-    <FlatList horizontal showsHorizontalScrollIndicator={false} 
+    <FlatList showsHorizontalScrollIndicator={false} 
       keyExtractor={(friend)=> friend.name} data={friends} 
-      renderItem={({item})=> (<Text style={styles.text}>{item.name}</Text>)
+      renderItem={({item})=> (<Text style={styles.text}>{item.name} - Age {item.age}</Text>)
     }/>
   )
 }
