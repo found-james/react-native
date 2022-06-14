8adba893adff69024777a122388ced9d9ab252f3
James Suescun <james.steven2318@gmail.com>
2022-06-14 18:25:35 -0400

    use navigation prop to link to other screens

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = () => {
+const HomeScreen = (props) => {
+
   return (
     <View>
       <Text style={styles.text}>DeKabra</Text>
-      <Button title="Go to components demo" onPress={()=> console.log("pressed")}/>
+      <Button title="Go to components demo" onPress={()=> props.navigation.navigate("Components")}/>
       <TouchableOpacity onPress={()=> console.log("touchableOpButton")}>
           <Text>Go to list demo</Text>
       </TouchableOpacity>
